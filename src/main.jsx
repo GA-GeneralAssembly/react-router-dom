f74ddfdbab0d831b3bd26b4605f10a733f64619c
HussainWorld <hussain.university.poly@gmail.com>
2025-02-03 00:34:06 +0300

    done

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter } from 'react-router';
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+  </StrictMode>,
+)
+
+
+// Nest the App component inside the BrowserRouter component to enable routing:
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
